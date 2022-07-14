2b22017d3de7d96ac49da73da317e8681174a349
Bonbon315 <n202u123@gmail.com>
2022-07-14 14:28:06 +0900

    18번 예제

--- 실습문제풀이/18. 알파벳 등장 갯수 구하기.py
+++ 실습문제풀이/18. 알파벳 등장 갯수 구하기.py
@@ -1,10 +1,12 @@
 word = input().lower()
 
 dic = {}
+
 for i in word:
-    if i not in dic:
-        dic[i] = 1
-    else:
-        dic[i] += 1
+    dic[i] = 0
+
+for i in word:
+    dic[i] += 1
+
 for k, v in dic.items():
     print(k, v)
