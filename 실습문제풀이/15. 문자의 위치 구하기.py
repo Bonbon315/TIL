92a4fd005bb57c873bb37d21de9f85a9ab1d96e5
Bonbon315 <n202u123@gmail.com>
2022-07-14 15:05:45 +0900

    15번 예제 추가문제 업데이트

--- 실습문제풀이/15. 문자의 위치 구하기.py
+++ 실습문제풀이/15. 문자의 위치 구하기.py
@@ -1,8 +1,8 @@
-word = 'apple'
+word = input()
 
 apos = 0
 for i in word:
-    if i=='a':
+    if i == 'a':
         break
     apos += 1
 
@@ -10,3 +10,13 @@ if apos>=len(word):
     print(-1)
 else:
     print(apos)
+
+
+#추가 문제
+word2 = input()
+
+apos2 = 0
+for i in word2:
+    if i == 'a':
+        print(apos2, end=' ')
+    apos2 += 1
