5c859edcaaa4da9344a75d552946b0a5fd12a2cb
Bonbon315 <n202u123@gmail.com>
2022-07-17 19:30:07 +0900

    baekjoon 02

---
+++ baekjoon/step_by_step/조건문/3dice.py
@@ -0,0 +1,19 @@
+a, b, c = map(int, input().split())
+prize = 0
+if a == b == c:
+    prize = 10000 + a * 1000
+elif a == b:
+    prize = 1000 + a * 100
+elif a == c:
+    prize = 1000 + a * 100
+elif b == c:
+    prize = 1000 + b * 100
+else:
+    if a >= b and a >= c:
+        prize = a * 100
+    elif b >= a and b >= c:
+        prize = b * 100
+    else:
+        prize = c * 100
+
+print(prize)
