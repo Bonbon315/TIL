86f5e92c64b9ca1a4532c21ce59d2a567152bc0e
Bonbon315 <n202u123@gmail.com>
2022-07-18 13:33:43 +0900

    quick fix

--- 실습문제풀이/19. 숫자의 길이 구하기.py
+++ 실습문제풀이/19. 숫자의 길이 구하기.py
@@ -2,10 +2,11 @@ number = int(input())
 
 result = 0
 
+if number == 0:
+    result = 1
 while number > 0:
     number = number // 10
     result += 1
-if number == 0:
-    result = 1
+
 
 print(result)
