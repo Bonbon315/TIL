3f7d567477f2370f59c4721c06363b40692eca62
Bonbon315 <n202u123@gmail.com>
2022-07-18 13:47:38 +0900

    예제 06. [오류 해결] 1부터 N까지의 2의 곱 저장하기

---
+++ 실습문제풀이/예제 06. [오류 해결] 1부터 N까지의 2의 곱 저장하기.py
@@ -0,0 +1,15 @@
+# N = 10
+# answer = ()
+# for number in range(N + 1):
+#     answer.append(number * 2)
+
+# print(answer)
+
+# 리스트는 []으로 선언해야한다. tuple엔 append 함수를 쓸 수 없다.
+
+N = 10
+answer = []
+for number in range(N + 1):
+    answer.append(number * 2)
+
+print(answer)
