c8cd88573fbef10801e91105e5e9f2c3b7edba3c
Bonbon315 <n202u123@gmail.com>
2022-07-21 14:19:08 +0900

    SWEA

--- SWEA/3일차/1288.py
+++ SWEA/3일차/1288.py
@@ -2,7 +2,14 @@ import sys
 sys.stdin = open("1288_input.txt", "r")
 
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     N = int(input())
-    
+    numbers = [0] * 10
+
+    count = 1
+    while 0 in numbers:
+        num = str(N * count)
+        for i in range(len(num)):
+            numbers[int(num[i])] += 1
+        count += 1
+    print(f'#{test_case} {num}')
