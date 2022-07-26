3713c78640e747a58fa064d8b54f37ac5b49b603
Bonbon315 <n202u123@gmail.com>
2022-07-26 16:01:59 +0900

    1936

---
+++ SWEA/D1/1936. 1대1 가위바위보.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open("1936. 1대1 가위바위보.txt")
+
+A, B = map(int, input().split())
+
+if (A == 1 and B == 3) or (A == 2 and B == 1) or (A == 3 and B == 2) :
+    print('A')
+else:
+    print('B')
