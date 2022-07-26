c15ac6f88c707c389b8dd5ccad2f76661c55afdf
Bonbon315 <n202u123@gmail.com>
2022-07-26 15:38:34 +0900

    SWEA 난이도별 정리

--- SWEA/testpad.py
+++ SWEA/testpad.py
@@ -1,4 +1,10 @@
-a = 123
+numbers = [1, 2, 4, 6, 8, 10, 11]
 
-if '1' in str(a):
-    print('yes')
+total = 0 
+count = 0
+
+for i in range(1, len(numbers)-1):
+    total += numbers[i]
+    count += 1
+
+print(total // count)
